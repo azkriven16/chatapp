60b0e9879781797eee00f6b8e0244fcc96149de3
Euger Bonete <azkriven17@gmail.com>
2025-08-27 12:53:29 +0800

    chat container

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useRef } from "react";
 import assets, { messagesDummyData } from "../assets/assets";
+import { formatMsgTime } from "../lib/utils";
 
 export const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+  const scrollEnd = useRef();
+
+  useEffect(() => {
+    if (scrollEnd.current) {
+      scrollEnd.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
   return selectedUser ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
       {/* header */}
@@ -59,10 +68,33 @@ export const ChatContainer = ({ selectedUser, setSelectedUser }) => {
                 alt=""
                 className="w-7 rounded-full"
               />
-              <p className="text-gray-500">{msg.createdAt}</p>
+              <p className="text-gray-500">{formatMsgTime(msg.createdAt)}</p>
             </div>
           </div>
         ))}
+
+        <div ref={scrollEnd}></div>
+      </div>
+
+      {/* bottom input area */}
+
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+          <input
+            type="text"
+            placeholder="Send a message"
+            className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400"
+          />
+          <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+          <label htmlFor="image">
+            <img
+              src={assets.gallery_icon}
+              className="w-5 mr-2 cursor-pointer"
+              alt=""
+            />
+          </label>
+        </div>
+        <img src={assets.send_button} className="w-7 cursor-pointer" alt="" />
       </div>
     </div>
   ) : (
