5c9744b2475c4b77f9358ed6e524f8d877c250e4
Euger Bonete <azkriven17@gmail.com>
2025-08-30 15:54:50 +0800

    updated server export for vercel

--- server/server.js
+++ server/server.js
@@ -45,6 +45,10 @@ app.use("/api/messages", messageRouter);
 // connect to db
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log(`server is running on port:${PORT}`));
+}
 
-server.listen(PORT, () => console.log(`server is running on port:${PORT}`));
+// export for vercel
+export default server;
